e26f2f716f90de08548730b6ace9ede6ee99042f
Venkatraman <ramsunvtech@gmail.com>
2015-11-27 04:25:47 +0530

    Commented the Base View

--- app/js/view/shared/Base.js
+++ app/js/view/shared/Base.js
@@ -1,14 +1,27 @@
+// Base View.
+
 define([
   'backbone'
 ], function (Backbone) {
 
+  'use strict';
+
   return Backbone.View.extend({
     
+    /**
+     *  Trigger on every View Instantiation which is extending from Base View.
+     *  Process the Template using underscore template `_.template` Method.
+     *  Finally calling the Render Method.
+     */
     initialize: function () {
       this.processTemplate();
       this.render();
     },
 
+    /**
+     *  Process the given `this.template` and `this.templateHelpers()` (template data).
+     *  Set it to the View Element HTML.
+     */
     processTemplate: function () {
       if( this.template ) {
         var tpl = _.template(this.template),
